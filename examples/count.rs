e2c315f66f58db13bd4ad8876c5b005ae4af118c
zzmp <zmp@umich.edu>
2014-07-01 11:39:42 -0700

    (refactor) Updated for iron's flattening.

--- examples/count.rs
+++ examples/count.rs
@@ -3,9 +3,8 @@ extern crate iron;
 extern crate session;
 
 use std::io::net::ip::{SocketAddr, Ipv4Addr};
-use iron::{Iron, ServerT, Chain, Request, Response, Alloy};
+use iron::{Iron, Server, Chain, Request, Response, Alloy, Status, Continue, FromFn};
 use iron::mixin::Serve;
-use iron::middleware::{Status, Continue, FromFn};
 use session::{Sessions, SessionStore, HashSessionStore, Session};
 
 // Echo the sessioned count to the client
@@ -22,7 +21,7 @@ fn get_count(req: &mut Request, res: &mut Response, alloy: &mut Alloy) -> Status
 }
 
 fn main() {
-    let mut server: ServerT = Iron::new();
+    let mut server: Server = Iron::new();
     server.chain.link(Sessions::new(id_from_socket_addr, HashSessionStore::<SocketAddr, u32>::new()));
     server.chain.link(FromFn::new(get_count));
     server.listen(Ipv4Addr(127, 0, 0, 1), 3000);
